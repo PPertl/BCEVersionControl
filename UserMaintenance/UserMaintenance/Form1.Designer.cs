8e8c45a868cc243207fd923d95a779358b2db851
Péter Pertl <peter.pertl@stud.uni-corvinus.hu>
2020-09-24 15:11:24 +0200

    Fájlbaírás

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.txtFullName = new System.Windows.Forms.TextBox();
             this.lblFullName = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.btnFajlbaIr = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listUsers
@@ -68,11 +69,22 @@
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
+            // btnFajlbaIr
+            // 
+            this.btnFajlbaIr.Location = new System.Drawing.Point(506, 89);
+            this.btnFajlbaIr.Name = "btnFajlbaIr";
+            this.btnFajlbaIr.Size = new System.Drawing.Size(75, 23);
+            this.btnFajlbaIr.TabIndex = 6;
+            this.btnFajlbaIr.Text = "button1";
+            this.btnFajlbaIr.UseVisualStyleBackColor = true;
+            this.btnFajlbaIr.Click += new System.EventHandler(this.btnFajlbaIr_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFajlbaIr);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.lblFullName);
             this.Controls.Add(this.txtFullName);
@@ -90,6 +102,7 @@
         private System.Windows.Forms.TextBox txtFullName;
         private System.Windows.Forms.Label lblFullName;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnFajlbaIr;
     }
 }
 
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,9 +19,10 @@ namespace UserMaintenance
         {
 
             InitializeComponent();
-            lblFullName.Text = Resource.FullName; 
-            
+            lblFullName.Text = Resource.FullName;
+
             btnAdd.Text = Resource.Add;
+            btnFajlbaIr.Text = Resource.WriteToFile;
 
             // listbox1
             listUsers.DataSource = users;
@@ -33,9 +35,33 @@ namespace UserMaintenance
             var u = new User()
             {
                 FullName = txtFullName.Text
-                
+
             };
             users.Add(u);
+        }
+
+        private void btnFajlbaIr_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Title = "Save";
+            saveDialog.Filter = "Text Files (*.txt)|*.txt" + "|" +
+                                "Image Files (*.png;*.jpg)|*.png;*.jpg" + "|" +
+                                "All Files (*.*)|*.*";
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream s = File.Open(saveDialog.FileName, FileMode.CreateNew))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    foreach (User item in users)
+                    {
+                        sw.WriteLine("teljes név: " + item.FullName);
+                        sw.WriteLine("ID: " + item.ID);
+                        sw.WriteLine();
+                    }
+
+                }
+            }
+
         }
     }
 }
