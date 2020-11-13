1d649fcd4cb2047a8d199e83f30d7f6fa35ef57f
Péter Pertl <peter.pertl@stud.uni-corvinus.hu>
2020-11-13 15:13:53 +0100

    Termékválasztó gombok

--- UserMaintenance/MikulasGyar/Form1.Designer.cs
+++ UserMaintenance/MikulasGyar/Form1.Designer.cs
@@ -32,11 +32,14 @@
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblNext = new System.Windows.Forms.Label();
+            this.Car_button = new System.Windows.Forms.Button();
+            this.Ball_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
-            this.mainPanel.Location = new System.Drawing.Point(0, 74);
+            this.mainPanel.Location = new System.Drawing.Point(0, 363);
             this.mainPanel.Name = "mainPanel";
             this.mainPanel.Size = new System.Drawing.Size(784, 100);
             this.mainPanel.TabIndex = 0;
@@ -53,15 +56,48 @@
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // lblNext
+            // 
+            this.lblNext.AutoSize = true;
+            this.lblNext.Location = new System.Drawing.Point(13, 199);
+            this.lblNext.Name = "lblNext";
+            this.lblNext.Size = new System.Drawing.Size(68, 13);
+            this.lblNext.TabIndex = 1;
+            this.lblNext.Text = "Coming next:";
+            // 
+            // Car_button
+            // 
+            this.Car_button.Location = new System.Drawing.Point(5, 65);
+            this.Car_button.Name = "Car_button";
+            this.Car_button.Size = new System.Drawing.Size(75, 23);
+            this.Car_button.TabIndex = 2;
+            this.Car_button.Text = "CAR";
+            this.Car_button.UseVisualStyleBackColor = true;
+            this.Car_button.Click += new System.EventHandler(this.Car_button_Click);
+            // 
+            // Ball_button
+            // 
+            this.Ball_button.Location = new System.Drawing.Point(109, 65);
+            this.Ball_button.Name = "Ball_button";
+            this.Ball_button.Size = new System.Drawing.Size(75, 23);
+            this.Ball_button.TabIndex = 3;
+            this.Ball_button.Text = "BALL";
+            this.Ball_button.UseVisualStyleBackColor = true;
+            this.Ball_button.Click += new System.EventHandler(this.Ball_button_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.Ball_button);
+            this.Controls.Add(this.Car_button);
+            this.Controls.Add(this.lblNext);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +106,9 @@
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Label lblNext;
+        private System.Windows.Forms.Button Car_button;
+        private System.Windows.Forms.Button Ball_button;
     }
 }
 
--- UserMaintenance/MikulasGyar/Form1.cs
+++ UserMaintenance/MikulasGyar/Form1.cs
@@ -15,12 +15,16 @@ namespace MikulasGyar
     public partial class Form1 : Form
     {
         private List<Toy> _toys = new List<Toy>();
-
+        private Toy _nextToy;
         private IToyFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set 
+            {
+                _factory = value;
+                DisplayNext();
+            }
         }
         
         public Form1()
@@ -55,6 +59,26 @@ namespace MikulasGyar
             }
         }
 
-        
+        private void Car_button_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void Ball_button_Click(object sender, EventArgs e)
+        {
+            Factory =  new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy !=null)
+            {
+                Controls.Remove(_nextToy);
+            }
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = lblNext.Top + lblNext.Height + 20;
+            _nextToy.Left = lblNext.Left;
+            Controls.Add(_nextToy);
+        }
     }
 }
