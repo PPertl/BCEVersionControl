4025e0f9cf5357689e2557a98a6dd8978eacc2b9
Péter Pertl <peter.pertl@stud.uni-corvinus.hu>
2020-11-13 15:21:34 +0100

    Színválasztó gomb

--- UserMaintenance/MikulasGyar/Form1.Designer.cs
+++ UserMaintenance/MikulasGyar/Form1.Designer.cs
@@ -33,8 +33,9 @@
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
             this.lblNext = new System.Windows.Forms.Label();
-            this.Car_button = new System.Windows.Forms.Button();
-            this.Ball_button = new System.Windows.Forms.Button();
+            this.btnCar = new System.Windows.Forms.Button();
+            this.btnBall = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -65,33 +66,44 @@
             this.lblNext.TabIndex = 1;
             this.lblNext.Text = "Coming next:";
             // 
-            // Car_button
+            // btnCar
             // 
-            this.Car_button.Location = new System.Drawing.Point(5, 65);
-            this.Car_button.Name = "Car_button";
-            this.Car_button.Size = new System.Drawing.Size(75, 23);
-            this.Car_button.TabIndex = 2;
-            this.Car_button.Text = "CAR";
-            this.Car_button.UseVisualStyleBackColor = true;
-            this.Car_button.Click += new System.EventHandler(this.Car_button_Click);
+            this.btnCar.Location = new System.Drawing.Point(5, 65);
+            this.btnCar.Name = "btnCar";
+            this.btnCar.Size = new System.Drawing.Size(75, 23);
+            this.btnCar.TabIndex = 2;
+            this.btnCar.Text = "CAR";
+            this.btnCar.UseVisualStyleBackColor = true;
+            this.btnCar.Click += new System.EventHandler(this.Car_button_Click);
             // 
-            // Ball_button
+            // btnBall
             // 
-            this.Ball_button.Location = new System.Drawing.Point(109, 65);
-            this.Ball_button.Name = "Ball_button";
-            this.Ball_button.Size = new System.Drawing.Size(75, 23);
-            this.Ball_button.TabIndex = 3;
-            this.Ball_button.Text = "BALL";
-            this.Ball_button.UseVisualStyleBackColor = true;
-            this.Ball_button.Click += new System.EventHandler(this.Ball_button_Click);
+            this.btnBall.Location = new System.Drawing.Point(109, 65);
+            this.btnBall.Name = "btnBall";
+            this.btnBall.Size = new System.Drawing.Size(75, 23);
+            this.btnBall.TabIndex = 3;
+            this.btnBall.Text = "BALL";
+            this.btnBall.UseVisualStyleBackColor = true;
+            this.btnBall.Click += new System.EventHandler(this.Ball_button_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Tomato;
+            this.btnColor.Location = new System.Drawing.Point(229, 124);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 4;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.Ball_button);
-            this.Controls.Add(this.Car_button);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnBall);
+            this.Controls.Add(this.btnCar);
             this.Controls.Add(this.lblNext);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
@@ -107,8 +119,9 @@
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
         private System.Windows.Forms.Label lblNext;
-        private System.Windows.Forms.Button Car_button;
-        private System.Windows.Forms.Button Ball_button;
+        private System.Windows.Forms.Button btnCar;
+        private System.Windows.Forms.Button btnBall;
+        private System.Windows.Forms.Button btnColor;
     }
 }
 
